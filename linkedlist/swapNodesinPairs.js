4cc6dc7eed8c27425f97d4aa0290c29c8ff640a2
Prakash Singh Kotiya <prakashkotiya100@gmail.com>
2026-09-07 04:54:02 +0530

    linkedlist

--- linkedlist/swapNodesinPairs.js
+++ linkedlist/swapNodesinPairs.js
@@ -1,3 +1,31 @@
+// 24. Swap Nodes in Pairs
+// Given a linked list, swap every two adjacent nodes and return its head. 
+// You must solve the problem without modifying the values in the list's nodes (i.e., only nodes themselves may be changed.)
+// Input: head = [1,2,3,4]  Output: [2,1,4,3]
+
+//logic : (iterative approach : creating a dummy node (sentinal node) using 3 pointer prev cur and current.next 
+// swapping the prev.next with cur.next and cur.next with cur.next.next and then pointing cur.next pointer to cur . make sure 
+// the main logic is  of pointing to node and swapping the nodes that is happeniing here , and then iterating the loop by changing 
+// the pointers position..)
+
+//(reccursive approach : calling function recuursively and passing the head.next.next.next(r.next) value that is pointing to
+//  head.next(l) and then pointing the r to l (head.next.next(r) to head.next (curr)) and the nodes are getting swapped )
+
+//reccursive approach :
+var swapPairs = function(head) {
+    if(!head || !head.next ) return head
+    let l = head
+    let r = head.next
+    l.next = swapPairs(r.next)
+     r.next = l
+     return r
+   
+};
+
+
+
+
+//iterative approach
 var swapPairs = function(head) {
     if(!head || !head.next) return head
     let prev = new ListNode()
